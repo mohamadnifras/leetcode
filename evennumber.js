b592a6294ab4a2b3e5726d59177119792422f1de
mohamadnifras <mohamadnifras2004@gmail.com>
2024-09-13 09:21:11 +0530

    13/9/24

---
+++ evennumber.js
@@ -0,0 +1,8 @@
+let number =6;
+
+if(number %2===0){
+    console.log("The Even Number"); 
+}else{
+    console.log("the Odd Number");
+    
+}
---
+++ ternaryoperater.js
@@ -0,0 +1,7 @@
+let mark=40;
+let result = (mark>=40&&mark<=50)? "The Gread is A":
+(mark>=30&&mark<=39)? "The Gread is B":
+(mark>=20&&mark<=29)?"The Gread is C":
+(mark>=0&&mark<=19)?"Student has failed":
+"invalid mark"
+console.log(result);
