97b190cb4cc79252234cc1cb8f8ab5b53c75fe6e
mohamadnifras <mohamadnifras2004@gmail.com>
2024-10-24 10:28:38 +0530

    twoSum

--- twosum.js
+++ twosum.js
@@ -1,17 +1,16 @@
-
 var twoSum = function(nums, target) {
-    let number=[]
-    for(let i=0;i<nums.length;i++){
-      for(let j=i+1;j<nums.length;j++){
-         if(nums[i] + nums[j] === target){
-           return  number=[i,j]
-         }
-      }
-       
-    }
    
-  };
- console.log(twoSum([2,7,11,15],26));
+  for(let i=0;i<=nums.length;i++){
+   for(let j=i+1;j<nums.length;j++){
+    if(nums[i]+nums[j] === target){
+        return [i, j]
+    }
+   }
+  }
+};
+console.log(twoSum([2,7,3,4,6],9));
+
+
   
   
   
