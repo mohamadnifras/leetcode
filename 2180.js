10bd183b9f247645c5875af84c466786f37660a8
mohamadnifras <mohamadnifras2004@gmail.com>
2024-11-18 10:54:05 +0530

    2180

--- 2180.js
+++ 2180.js
@@ -1,21 +1,37 @@
 var countEvenDigitSum = function(num) {
     let count = 0;
+
  
-   
     for (let i = 1; i <= num; i++) {
         
         let digitSum = i.toString()
             .split('')
             .map(Number)
             .reduce((sum, digit) => sum + digit, 0);
- 
-       
+
+      
         if (digitSum % 2 === 0) {
             count++;
         }
     }
+
+    return count; 
+};
+
+console.log(countEvenDigitSum(30));  
  
-    return digit;
- };
+
+
+
+    
+    
  
- console.log(countEvenDigitSum(4));
+   
+ 
+  
+
+ 
+ 
+ 
+
+
