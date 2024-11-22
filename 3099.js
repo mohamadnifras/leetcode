687140506f0c802a3cd506f9b72fe8014fab14fc
mohamadnifras <mohamadnifras2004@gmail.com>
2024-11-22 10:44:16 +0530

    3099

---
+++ 3099.js
@@ -0,0 +1,13 @@
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+   let sumDigit = x.toString().split('').reduce((sum,digit)=> sum + Number(digit),0)
+   if(x%sumDigit ===0){
+    return sumDigit
+   }else{
+    return -1
+   }
+};
+
+console.log(sumOfTheDigitsOfHarshadNumber(18));
+
+
+
