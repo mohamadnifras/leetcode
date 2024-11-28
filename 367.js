c77a43f7f516e1ca5c1ee9dfce04bb9ddfee6cfa
mohamadnifras <mohamadnifras2004@gmail.com>
2024-11-28 11:47:22 +0530

    367

---
+++ 367.js
@@ -0,0 +1,11 @@
+var isPerfectSquare = function(num) {
+    let squaroot = Math.sqrt(num)
+    let perfectSquare = Number.isInteger(squaroot)
+    return perfectSquare
+};
+console.log(isPerfectSquare(14));
+
+
+
+
+
