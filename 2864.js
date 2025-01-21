c3ff783cd5f7277ce89f259245fc764703e029ec
mohamadnifras <mohamadnifras2004@gmail.com>
2025-01-21 11:17:12 +0530

    2864

---
+++ 2864.js
@@ -0,0 +1,12 @@
+var maximumOddBinaryNumber = function (s) {
+    count1 = [...s].filter((char) => char === '1').length
+    count0 = [...s].filter((char) => char === '0').length
+
+    if (count1 === 0) {
+        return "";
+    }
+
+    const result = '1'.repeat(count1 - 1) + '0'.repeat(count0) + 1
+    return result
+};
+console.log(maximumOddBinaryNumber("010"));
