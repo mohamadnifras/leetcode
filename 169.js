ba074aa0f97e9afe03bfd090af4c8005e606b413
mohamadnifras <mohamadnifras2004@gmail.com>
2025-06-23 15:06:40 +0530

    majorityElement

---
+++ 169.js
@@ -0,0 +1,19 @@
+var majorityElement = function(nums) {
+    let count = 0;
+  let candidate = null;
+
+  for (let num of nums) {
+    console.log(num,"for")
+    if (count === 0) {
+      candidate = num;
+    }
+   
+    // console.log(candidate,"candidate")
+    count += (num === candidate) ? 1 : -1;
+  }
+
+  return candidate;
+};
+let nums = [3,2,3]
+
+console.log(majorityElement(nums))
---
+++ 2.js
@@ -0,0 +1,11 @@
+// var addTwoNumbers = function(l1, l2) {
+//     let a = l1.reverse()
+//     let b = l2.reverse()
+//     let ab= a + b;
+//     return 
+// };
+// l1 = [2,4,3], l2 = [5,6,4]
+// console.log(addTwoNumbers(l1,l2))
+
+l1 = [2,4,3], l2 = [5,6,4]
+console.log(Math.max(l1.length, l2.length))
