eb038815f96a4451851b841cea895c7576f18b3b
mohamadnifras <mohamadnifras2004@gmail.com>
2025-06-26 10:01:39 +0530

    longestCommonPrefix

---
+++ 14.js
@@ -0,0 +1,14 @@
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0]
+    if(!strs.length)return "";
+    for(let i = 1; i< strs.length; i++){
+        while(strs[i].indexOf(prefix) !==0){
+            prefix = prefix.slice(0,-1)
+        }
+        if(prefix === "")return ""
+    }
+    return prefix
+
+};
+const strs = ["flower","flow","flight"]
+console.log(longestCommonPrefix(strs))
